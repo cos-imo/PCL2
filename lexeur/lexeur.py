1ee83a3671d9ad9682d2f58e2fba05a67f364243
ugo <ugo.gosso@gmail.com>
2023-11-22 13:43:39 +0100

    error handling

--- lexeur/lexeur.py
+++ lexeur/lexeur.py
@@ -6,12 +6,10 @@ def lexical_analysis(code_source: str) -> (tuple[list, dict] or bool):
     token, lexical_table = scan(code_source)
 
     if lexical_table[5]:
-        for pb in lexical_table[5]:
-            if pb == "rem":
-                raise RemWithoutSpace(pb[1])
-            else:
-                raise ForbiddenAscii(pb[1], pb[0])
-
-        return False
+        pb = lexical_table[5][0]
+        if pb == "rem":
+            raise RemWithoutSpace(pb[1])
+        else:
+            raise ForbiddenAscii(pb[1], pb[0])
     else:
         return token, lexical_table
