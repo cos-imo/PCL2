224b9aaa0ac9cbf330d2c183d00323f633525839
DUPRE Romain <romain.dupre@telecomnancy.eu>
2023-12-12 12:07:23 +0100

    ajout table lexicale

---
+++ parseur/table_syntaxique.py
@@ -0,0 +1,11 @@
+# Création de la table syntaxique
+table_syntaxique = {
+    'dictionnaire1': {
+        'cle1': 'valeur1',
+        'cle2': 'valeur2'
+    },
+    'dictionnaire2': {
+        'cle3': 'valeur3',
+        'cle4': 'valeur4'
+    }
+}
