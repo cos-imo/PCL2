182ed4b6b2b2b5d1ad3d0335de67b4885577b3e0
ugo <ugo.gosso@gmail.com>
2024-01-17 17:47:58 +0100

    lexer error

--- lexeur/scan/scanner.py
+++ lexeur/scan/scanner.py
@@ -4,11 +4,12 @@ from .scanUtils import scan_operator, scan_int, scan_key_identifier
 def scanner(source_code: str) -> "tuple[list, dict]":
     # init the lexical table with 0 as keywords, 1 as operator, 2 as syntax operator, 3 as identifier, 4 as constant number, 5 invalid char
     lexical_table = {
-        0: ["access", "and", "begin", "else", "elsif", "end", "false", "for", "function", "if", "in", "is", 
+        0: ["access", "and", "begin", "else", "elsif", "end", "false", "for", "function", "if", "in", "is",
             "loop", "new", "not", "null", "or", "out", "procedure", "record", "rem", "return", "reverse",
-            "then", "true", "type", "use", "while", "with", "character", "kteger","ada.text_io","eof"],
-        1: ["+", "-", "*", "/", "<", ">", "<=", ">=", "=", "/=", "=>", ".", ":=", "..", "-"], # le second moins est le moins unaire
-        2: ["!", chr(34), "#", "$", "%", "&", "'", "(", ")", ",", ":", ";", "?", "@", "[", chr(92), "]", "^", 
+            "then", "true", "type", "use", "while", "with", "character", "kteger", "ada.text_io", "eof"],
+        1: ["+", "-", "*", "/", "<", ">", "<=", ">=", "=", "/=", "=>", ".", ":=", "..", "-"],
+        # le second moins est le moins unaire
+        2: ["!", chr(34), "#", "$", "%", "&", "'", "(", ")", ",", ":", ";", "?", "@", "[", chr(92), "]", "^",
             "_", "`", "{", "|", "}", "~"],
         3: [], 4: [], 5: []}
 
@@ -45,6 +46,13 @@ def scanner(source_code: str) -> "tuple[list, dict]":
         # check if we have a key word, operator, constant number or ...
         if actual.isalpha():
             type_, value, position = scan_key_identifier(source_code, position, lexical_table[0])
+
+            for l in value:
+                if ord(l) not in range(32, 127) and l != "\n":
+                    type_, value, position = 5, source_code[position], position + 1
+                    lexical_table[type_].append((value, line))
+                    token.append((type_, len(lexical_table[5]), line))
+                    continue
         elif actual.isdigit():
             type_, value, position = scan_int(source_code, position)
         else:
@@ -58,7 +66,7 @@ def scanner(source_code: str) -> "tuple[list, dict]":
 
         # Différenciation moins et moins unaire
         if type_ == 1 and lexical_table[type_].index(value) == 1:
-            if token[-1][0] == 1 or (token[-1][0]==2 and token[-1][1] in ["(", ",", ":", ";", "[", "^", chr(92)]) :
+            if token[-1][0] == 1 or (token[-1][0] == 2 and token[-1][1] in ["(", ",", ":", ";", "[", "^", chr(92)]):
                 value = 14
 
         # add the new token in the list
--- test_PCL1_error.py
+++ test_PCL1_error.py
@@ -4,11 +4,23 @@ from lexeur.lexeur import lexical_analysis
 
 
 class MyTestCase(unittest.TestCase):
-    def test_lexeur_error(self):
-        with open("tests/test_lexeur/forbiddenAscii") as f:
+    def test_lexeur_error_forbidAscii(self):
+        with open("tests/test_lexeur/forbiddenAscii.txt") as f:
             source_code = f.read()
 
+        print(lexical_analysis(source_code))
 
+    def test_lexeur_error_remWS(self):
+        with open("tests/test_lexeur/remWithoutSpace.txt") as f:
+            source_code = f.read()
+
+        print(lexical_analysis(source_code))
+
+    def test_lexeur_error_idenBN(self):
+        with open("tests/test_lexeur/identBeginNumber.txt") as f:
+            source_code = f.read()
+
+        print(lexical_analysis(source_code))
 
 
 if __name__ == '__main__':
