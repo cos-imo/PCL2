5374f72dee847de7d204256695ff2a8d6c742383
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2024-02-08 15:48:03 +0100

    [feat] Mise en place des symboles

---
+++ table_des_symboles/symbole.py
@@ -0,0 +1,27 @@
+import sys
+
+class symbole:
+
+    def __init__(self, name, type_entree="None", size=0, Dimension=0, ligne_declaration=None, ligne_utilisation=None, address=None):
+        self.name = name
+        self.type = type_entree
+        self.size = size
+        self.Dimension = Dimension
+        self.ligne_declaration = ligne_declaration
+        self.ligne_utilisation  = ligne_declaration
+        self.address = address
+
+    def __repr__(self):
+        sys.stdout.write("\t => SYMBOLE\n")
+        sys.stdout.write("\t\tNom: " + str(self.name))
+        sys.stdout.write("\t\tType: " + str(self.type_entree))
+        sys.stdout.write("\t\tTaille: " + str(self.Dimension))
+        sys.stdout.write("\t\tDimension: " + str(self.ligne_declaration))
+        sys.stdout.write("\t\tLigne de déclaration: " + name)
+        utilisation_str = ""
+        if len(ligne_utilisation)>1:
+            for ligne in ligne_utilisation:
+                utilisation_str += ligne
+            sys.stdout.write("\t\tLignes d'utilisation: " + str(utilisation_str))
+        sys.stdout.write("\t\tLigne d'utilisation: " + str(ligne_utilisation[0]))
+        sys.stdout.write("Adresse mémoire: " + str(address))
