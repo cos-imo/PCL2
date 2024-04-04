95981740f71ce8450097454bf8ea10c387e7a259
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2024-04-04 16:25:02 +0200

    [feat] Ajout de commentaires sur le générateur d'assembleur

--- assembly/asm_generator/assembly_generator.py
+++ assembly/asm_generator/assembly_generator.py
@@ -3,19 +3,26 @@ import sys
 
 class assembly_generator:
 
-    def __init__(self):
+    def __init__(self, tds, tds_data):
+        # Variable contenant le numéro de ligne (du fichier assembleur) actuel
+        self.line_index = 0  
+
+        ### Création du fichier de code assembleur
+        ## Création du dossier s'il n'existe pas déjà au préalable
         if os.path.exists("asm_output"):
             pass
         else:
             os.mkdir("asm_output")
-        
+
+        ## Création du fichier
+        # On vérifie si le fichier n'existe pas déjà
         if os.path.exists("asm_output/output.s"):
             sys.stdout.write("[!] Attention! Un fichier 'output.s' est déjà présent dans le répertoire 'asm_output'.\n\tVoulez-vous tout de même lancer la génération de code assembleur?\n\tNOTE: Cela écrasera les données pré-existantes\n")
             self.file_exists()
         with open("assembly/program_template/program_template.s", "r") as file:
             self.data = file.readlines()
-        self.line_index = 0  
         self.add_function("test")
+        # On écrit dans le fichier
         self.write_file()
 
     def add_var(self, var_type, var_name):
@@ -35,6 +42,8 @@ class assembly_generator:
 
 
     def add_function(self, function_name):
+        function = [f"{function_name}:"]
+        self.data = self.data[:self.line_index] + function + self.data[self.line_index:]
         pass
         #Bonne chance hein
 
