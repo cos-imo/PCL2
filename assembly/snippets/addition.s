1bbd479d2922155cbcb9cc5921dc07b41da3a02a
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2024-04-10 00:24:12 +0200

    [feat] Code de l'addition (seul)

--- assembly/snippets/addition.s
+++ assembly/snippets/addition.s
@@ -1,24 +1,11 @@
-section .data
-    saved_eax dd 0
-    saved_ebx dd 0
-    result dd 0
+mov dword [saved_eax], eax
+mov dword [saved_ebx], ebx
 
-section .text
-    global _start
+pop eax
+pop ebx
+add eax, ebx
 
-_start:
-    mov dword [saved_eax], eax
-    mov dword [saved_ebx], ebx
+mov dword [result], eax
 
-    pop eax
-    pop ebx
-    add eax, ebx
-
-    mov dword [result], eax
-
-    mov eax, dword [saved_eax]
-    mov ebx, dword [saved_ebx]
-
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
+mov eax, dword [saved_eax]
+mov ebx, dword [saved_ebx]
