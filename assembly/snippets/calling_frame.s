562e5781b6f70fc6dfa355d5eba7836336744295
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2024-04-10 10:09:06 +0200

    [fix] formatage de size

--- assembly/snippets/calling_frame.s
+++ assembly/snippets/calling_frame.s
@@ -1,6 +1,6 @@
 push ebp
 mov ebp, esp
-sub esp, f"{size}" . ; remplacer size par la taille de la variable de retour
+sub esp, <SIZE> ; remplacer size par la taille de la variable de retour
   ; code de la fonction ici
 mov esp, ebp
 pop ebp
