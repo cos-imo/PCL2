25fac7641130e44074f0940bd3825348c5607600
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2024-05-07 23:06:25 +0200

    [test] Ajout d'un fichier de test boucle conditionnelle

---
+++ tests/test_if.adb
@@ -0,0 +1,12 @@
+-- Test de base qui doit foaire;nctionner
+with Ada.Text_IO ; use Ada.Text_IO ;
+procedure test_if_procedure is
+-- VARIABLES
+test_if_var : integer ;
+-- PROCEDURE PRINCIPALE
+begin
+  test_if_var := 2;
+  if test_if_var > 1 then
+    test_if_var := 0;
+  end if;
+end test_if_procedure ; eof
