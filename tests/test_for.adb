5104dbf051ccab671e4414a62102cbd1db9df9aa
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2024-05-07 23:06:05 +0200

    [test] Ajout d'un fichier de test boucle for

---
+++ tests/test_for.adb
@@ -0,0 +1,13 @@
+-- Test de base qui doit foaire;nctionner
+with Ada.Text_IO ; use Ada.Text_IO ;
+procedure test_for_procedure is
+-- VARIABLES
+test_for_var : integer ;
+I : integer;
+-- PROCEDURE PRINCIPALE
+begin
+  test_for_var := 0;
+  for I in 1 .. 5 loop
+    test_for_var = 1 ;
+  end loop;
+end test_for_procedure ; eof
