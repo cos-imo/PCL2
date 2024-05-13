46a1eb7b2303081745fd0b8600007bbf6895b495
JEANJACQUOT Thomas <thomas.jeanjacquot@telecomnancy.eu>
2024-05-13 13:04:16 +0200

    [fix] Whangement de RETURN en RETURN_SIZE dans le tempalte des fonctions

--- assembly/snippets/function_frame.s
+++ assembly/snippets/function_frame.s
@@ -1,7 +1,7 @@
 <FUNCTION_NAME>:
   push rbp
   mov rbp, rsp
-  sub rsp, <SIZE> ; remplacer size par la taille de la variable de retour
+  sub rsp, <RETURN_SIZE> ; remplacer size par la taille de la variable de retour
   <FUNCTION_CODE>
   mov rsp, rbp
   pop rbp
